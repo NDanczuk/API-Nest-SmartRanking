b3fc630e0c6ffe0153764ed5dd0f9af5c450e2f6
NDanczuk <nicolasdanc@gmail.com>
2024-08-22 16:44:06 -0300

    Validation pipes added

---
+++ src/players/pipes/player-validation-params.pipe.ts
@@ -0,0 +1,17 @@
+import {
+  ArgumentMetadata,
+  BadRequestException,
+  PipeTransform,
+} from '@nestjs/common'
+
+export class PlayersValidationParamsPipe implements PipeTransform {
+  transform(value: any, metadata: ArgumentMetadata) {
+    if (!value) {
+      throw new BadRequestException(
+        `Params value: ${metadata.data} must be informed!`,
+      )
+    }
+
+    return value
+  }
+}
--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,14 +1,26 @@
-import { Body, Controller, Delete, Get, Post, Query } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common'
 import { CreatePlayerDto } from './dto/create-player.dto'
 import { PlayersService } from './players.service'
 import { Player } from './interfaces/player.interface'
+import { PlayersValidationParamsPipe } from './pipes/player-validation-params.pipe'
 
 @Controller('players')
 export class PlayersController {
   constructor(private readonly playersService: PlayersService) {}
 
   @Get()
-  async listPlayers(@Query('email') email: string): Promise<Player[] | Player> {
+  async listPlayers(
+    @Query('email', PlayersValidationParamsPipe) email: string,
+  ): Promise<Player[] | Player> {
     if (email) {
       return await this.playersService.findByEmail(email)
     } else {
@@ -17,12 +29,15 @@ export class PlayersController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   async createUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
     await this.playersService.createUpdatePlayer(createPlayerDto)
   }
 
   @Delete()
-  async deletePlayer(@Query('email') email: string): Promise<void> {
+  async deletePlayer(
+    @Query('email', PlayersValidationParamsPipe) email: string,
+  ): Promise<void> {
     this.playersService.deletePlayer(email)
   }
 }
