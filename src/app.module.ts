7345f2a8a2671fb9b80e889104a9557e2772ae2d
NDanczuk <nicolasdanc@gmail.com>
2024-08-23 08:52:18 -0300

    implemented .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,9 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PlayersModule } from './players/players.module'
-import { PlayersController } from './players/players.controller'
-import { PlayersService } from './players/players.service'
 import { MongooseModule } from '@nestjs/mongoose'
 @Module({
-  imports: [
-    PlayersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://nicolasdanc3:banana32@cluster0.jqth8ro.mongodb.net/smartranking?retryWrites=true&w=majority',
-    ),
-  ],
+  imports: [PlayersModule, MongooseModule.forRoot(process.env.MONGO_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
