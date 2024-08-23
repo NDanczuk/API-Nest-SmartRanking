53e929a660a34ebd0d5c3f56bdd832e3d0448c9f
NDanczuk <nicolasdanc@gmail.com>
2024-08-23 10:05:50 -0300

    Categories module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,10 @@ import { AppService } from './app.service'
 import { PlayersModule } from './players/players.module'
 import { MongooseModule } from '@nestjs/mongoose'
 import { CathegoriesModule } from './cathegories/cathegories.module';
+import { CategoriesModule } from './categories/categories.module';
+import { CategoriesModule } from './categories/categories.module';
 @Module({
-  imports: [PlayersModule, MongooseModule.forRoot(process.env.MONGO_URI), CathegoriesModule],
+  imports: [PlayersModule, MongooseModule.forRoot(process.env.MONGO_URI), CathegoriesModule, CategoriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
